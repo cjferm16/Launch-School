f52953f5dfac7a8b1b05457d8c0b3c9ee353c820
cjferm16 <cjferm16@gmail.com>
2020-04-15 15:44:10 -0700

    Delete name.rb

--- Introduction to Programming with Ruby/The Basics/name.rb
+++
@@ -1,7 +0,0 @@
-puts "What is your first name?"
-firstName = gets.chomp
-puts "What is your last name?"
-lastName = gets.chomp
-puts "Hello " + firstName.to_s + " " + lastName.to_s + "!"
-
-10.times {puts firstName}
