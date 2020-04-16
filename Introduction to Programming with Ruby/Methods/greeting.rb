b05dd9a9236f9e2b9847f0199db359bbeb55e276
cjferm16 <cjferm16@gmail.com>
2020-04-15 17:33:51 -0700

    Methods - chapter exercises

---
+++ Introduction to Programming with Ruby/Methods/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  puts 'Your name is ' + name.to_s
+end
+
+greeting('Christian')
---
+++ Introduction to Programming with Ruby/Methods/return.rb
@@ -0,0 +1,7 @@
+def add_three(number)
+  return number + 3 #here, the program will return 7 because we are explicitly commanding it to return this line
+  number + 4
+end
+
+returnedValue = add_three(4)
+puts returnedValue
